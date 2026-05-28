d2c7f5785a62986e06df5679becb7217f189024a
VARMA NALLABOTHULA <126646746+varmababu@users.noreply.github.com>
2026-05-28 06:49:07 +0530

    Updated SecurityConfig

--- src/main/java/devops_tracker/config/SecurityConfig.java
+++ src/main/java/devops_tracker/config/SecurityConfig.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import org.springframework.http.HttpMethod;
+
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 
@@ -16,6 +18,13 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 
@@ -31,11 +40,17 @@ public class SecurityConfig {
 
         http
 
+                // ENABLE CORS
+                .cors(cors -> cors.configurationSource(
+                        corsConfigurationSource()
+                ))
+
+                // DISABLE CSRF
                 .csrf(csrf -> csrf.disable())
 
+                // AUTHORIZATION
                 .authorizeHttpRequests(auth -> auth
 
-                        // PUBLIC APIs
                         .requestMatchers(
 
                                 "/",
@@ -51,10 +66,15 @@ public class SecurityConfig {
 
                         ).permitAll()
 
-                        // EVERYTHING ELSE SECURED
+                        .requestMatchers(
+                                HttpMethod.OPTIONS,
+                                "/**"
+                        ).permitAll()
+
                         .anyRequest().authenticated()
                 )
 
+                // STATELESS SESSION
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(
                                 SessionCreationPolicy.STATELESS
@@ -74,8 +94,47 @@ public class SecurityConfig {
         return http.build();
     }
 
+    // CORS CONFIG
     @Bean
-    public AuthenticationManager authenticationManager(
+    public CorsConfigurationSource
+    corsConfigurationSource() {
+
+        CorsConfiguration configuration =
+                new CorsConfiguration();
+
+        configuration.setAllowedOrigins(List.of(
+
+                "http://localhost:5173",
+
+                "https://devops-pipeline-tracker.vercel.app"
+        ));
+
+        configuration.setAllowedMethods(List.of(
+                "GET",
+                "POST",
+                "PUT",
+                "DELETE",
+                "OPTIONS"
+        ));
+
+        configuration.setAllowedHeaders(List.of("*"));
+
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source =
+                new UrlBasedCorsConfigurationSource();
+
+        source.registerCorsConfiguration(
+                "/**",
+                configuration
+        );
+
+        return source;
+    }
+
+    @Bean
+    public AuthenticationManager
+    authenticationManager(
             AuthenticationConfiguration config)
             throws Exception {
 
