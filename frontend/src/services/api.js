d225065414e568ed7e2fa8c9a3a76525ad7c29b4
VARMA NALLABOTHULA <126646746+varmababu@users.noreply.github.com>
2026-05-28 06:53:40 +0530

    Fixed production API URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,17 +2,14 @@ import axios from "axios";
 
 const API = axios.create({
 
-  // CLOUD BACKEND URL
   baseURL:
     "https://devops-tracker-xceh.onrender.com",
 
-  // JSON HEADERS
   headers: {
     "Content-Type": "application/json",
   },
 });
 
-// ADD TOKEN AUTOMATICALLY
 API.interceptors.request.use(
 
   (config) => {
@@ -20,7 +17,6 @@ API.interceptors.request.use(
     const token =
       localStorage.getItem("token");
 
-    // ATTACH JWT TOKEN
     if (token) {
 
       config.headers.Authorization =
